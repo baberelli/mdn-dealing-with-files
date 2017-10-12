3ec12eff90c72d5b217917b26baf4c51aa1551fd
Caleb Cox <calebcox2010@gmail.com>
2017-10-12 18:54:26 -0500

    add alert to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+var name = "Caleb";
+var age = 26;
+var alive = true;
+
+/* adds a hello alert */
+var myVariable = document.querySelector('h1');
+alert('hello!');
