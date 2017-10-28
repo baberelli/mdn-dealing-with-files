e81bc8ea814d363ea576856ecebc47e5e31ce689
Caleb Cox <calebcox2010@gmail.com>
2017-10-28 13:30:08 -0500

    Add new pics and add click event

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,11 @@ var name = "Caleb";
 var age = 26;
 var alive = true;
 
-/* adds a hello alert */
+/* adds a hello alert pop-up */
 var myVariable = document.querySelector('h1');
 alert('hello!');
+
+//Creates annoying click pop-up
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
