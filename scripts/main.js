1bc5865e0a071df09ed747bdbaf1e70754c4ea3b
Caleb Cox <calebcox2010@gmail.com>
2017-10-28 15:11:06 -0500

    Add Change user button and click event to button

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
+//All Code is from mozilla's javaScript basics and modified to fit theme of website
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
-
+//Selects img elements
 var myImage = document.querySelector('img');
 
 //Adds an image change click event
@@ -12,4 +13,28 @@ myImage.onclick = function() {
     } else {
       myImage.setAttribute ('src','images/walrus-cleaners.jpeg');
     }
+}
+
+//Selects button and h1 elements
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+//Creates personalized greeting
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = "Don't let a walrus ruin your next party, " + myName;
+}
+
+//Initializes the prompt Code
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+//Sets and Changes the UserName
+myButton.onclick = function() {
+  setUserName();
 }
